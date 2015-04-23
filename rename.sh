ea75860ee885da838b8ac7ef43a2d09c09ad0609
Ryan <ryan@brightcloudengineering.com>
2015-04-22 23:19:57 -0400

    First layout of DDS signal generator.

--- rename.sh
+++ rename.sh
@@ -6,8 +6,8 @@ mv "$1-B_Mask.gbr" $1.gbs
 mv "$1-F_SilkS.gbr" $1.gto
 mv "$1-B_SilkS.gbr" $1.gbo
 mv "$1-Edge_Cuts.gbr" $1.gko
-mv "$1-Inner1_Cu.gbr" $1.g2l
-mv "$1-Inner2_Cu.gbr" $1.g3l
+mv "$1-In1_Cu.gbr" $1.g2l
+mv "$1-In2_Cu.gbr" $1.g3l
 mv "$1-F_Paste.gbr" $1.gtp
 mv "$1-B_Paste.gbr" $1.gbp
 cp $1.drl $1.xln
